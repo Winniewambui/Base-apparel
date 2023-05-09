c39c59e997d2ec2b83abb876197421df648a4b30
winniewambui <winniebwambui7@gmail.com>
2023-05-09 14:13:07 +0300

    fix bug in js

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ submitbutton.addEventListener('click', (e) => {
     }
 });
 function error (element, msg){
-    element.style.border = '1px solid  hsl(0, 93%, 68%)';
+    element.style.border = '2px solid red';
     const parent = element.parentElement;
     const p = parent.querySelector('p');
+    p.style.color = ' hsl(0, 93%, 68%)';
     p.textContent = msg;
-    p.style.visbility = 'visible';
+    p.style.visibility = 'visible';
 }
 function success (element){
     element.style.border = '1px solid hsl(0, 6%, 24%)';
     const parent = element.parentElement;
     const p = parent.querySelector('.errormessage');
-    p.style.visbility = 'hidden';
+    p.style.visibility = 'hidden';
 }
 
